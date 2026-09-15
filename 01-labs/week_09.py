bd1200be4a725f14987d8b6a8b3fd4cf729d8121
sisiprovis1 <milusheva.snezhina@gmail.com>
2026-09-15 07:42:28 +0000

    [human-only][feature] Avg Temp Bar Chart

--- 01-labs/week_09.py
+++ 01-labs/week_09.py
@@ -3,4 +3,18 @@ print("Hello, Week 9! 5 weeks to go!")
 import pandas as pd
 
 df = pd.read_csv("week_09_dataset.csv")
-print(df)
+
+#df_perth = df[df["city"] == "Perth"]
+#print(df_perth)
+
+# Average temperature for each city
+#print(df.groupby("city")["temperature"].mean())
+
+import matplotlib.pyplot as plt
+
+avg_temp = df.groupby("city")["temperature"].mean()
+
+avg_temp.plot(kind="bar", title="Fig 1. Average temperature by city")
+plt.ylabel("Temperature (°C)")
+plt.tight_layout()
+plt.savefig("avg_temp.png")
